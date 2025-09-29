be70acd85890b03002d4c6584a15d651d9615d28
Alef Santos <alef.santos@al.infnet.edu.br>
2025-09-29 19:50:03 -0300

    feat: update SkillsSection and SkillCard styles for improved layout and consistency

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,25 @@ import project2 from "./assets/project_2.png";
 function App() {
   return (
     <div className={style.App_container}>
+      
       <div className={style.App}>
+        
         <Header title={"Meu Portfólio"} />
+
         <ProfileCard
           picture={avatar}
           pictureName={"foto de perfil"}
           name={"Alef Silva"}
           description={"Graduando em Análise e Desenvolvimento de Sistemas"}
         />
+
         <SkillsSection
           title={"SKILLS"}
           skills={[
+            { title: "Flexbox & Grid Layout", nivel: "Acadêmico" },
             { title: "Mobile-First", nivel: "Acadêmico" },
             { title: "React", nivel: "Acadêmico" },
-            { title: "HTML", nivel: "Acadêmico" },
-            { title: "JavaScript", nivel: "Acadêmico" },
+            { title: "Git", nivel: "Prático" },
           ]}
         />
 
@@ -46,6 +50,7 @@ function App() {
             "Estou em transição de carreira e busco um estágio na área de desenvolvimento de software, onde possa aplicar de forma prática os conhecimentos adquiridos no curso de Análise e Desenvolvimento de Sistemas. Tenho grande interesse em trabalhar em projetos reais, contribuindo desde a concepção até a implementação de soluções, aprimorando minhas habilidades técnicas e minha capacidade de resolver problemas. Sou motivado, comprometido e estou sempre disposto a aprender; além do desenvolvimento de software, estudo inglês de forma autodidata, buscando alcançar uma segunda língua que me proporcione oportunidades internacionais no futuro. Estou constantemente aprimorando meus conhecimentos e pronto para agregar valor à equipe e aos projetos nos quais estiver envolvido."
           }
         />
+
         <ProjectsSection
           title={"MEUS PROJETOS"}
           projects={[
@@ -61,6 +66,7 @@ function App() {
             },
           ]}
         />
+        
       </div>
     </div>
   );
--- src/components/skillsSection/SkillsSection.jsx
+++ src/components/skillsSection/SkillsSection.jsx
@@ -5,9 +5,10 @@ export default function SkillsSection({ title, skills }) {
   return (
     <div className={styles.SkillsSection_container}>
       <h3 className={styles.SkillsSection_title}>{title}</h3>
+
       <section className={styles.SkillsSection}>
         {skills.map((skill, i) => (
-          <SkillCard 
+          <SkillCard
             key={i}
             title={skill.title}
             nivel={skill.nivel}
