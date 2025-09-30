19671a485ccc134d5e302a261a63f0b36d15b926
Alef Santos <alef.santos@al.infnet.edu.br>
2025-09-29 21:57:44 -0300

    style: add LinkedIn and GitHub logos to ProfileCard and update styles for social icons

--- src/components/profileCard/ProfileCard.jsx
+++ src/components/profileCard/ProfileCard.jsx
@@ -1,5 +1,7 @@
 import styles from "./ProfileCard.module.css";
 import useReveal from "../effects/UseReveal";
+import linkedinLogo from "../../assets/In-logo.png";
+import githubLogo from "../../assets/github-logo.png";
 
 export default function ProfileCard({ picture, pictureName, name, description }) {
     const [ref, isVisible] = useReveal();
@@ -11,6 +13,19 @@ export default function ProfileCard({ picture, pictureName, name, description })
             <div className={styles.profile_infoContainer}>
                 <h2 className={styles.profile_name}>{name}</h2>
                 <p className={styles.profile_description}>{description}</p>
+                <div className={styles.profile_socials}>
+                    <div className={styles.profile_linkedin}>
+                        <a href="https://www.linkedin.com/in/alefsilvasantos15/" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedinLogo} alt="LinkedIn" />
+                        </a>
+                    </div>
+
+                    <div className={styles.profile_github}>
+                        <a href="https://github.com/AlefSillva" target="_blank" rel="noopener noreferrer">
+                            <img src={githubLogo} alt="GitHub" />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     )
